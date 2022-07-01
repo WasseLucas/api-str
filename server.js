682f43d0fac19c1cef6373e72bbe4d5f8c60d833
WasseLucas <wasselucas.dev@gmail.com>
2022-06-30 22:20:16 -0300

    NormalizandoPorta-GerenciandoErroDoServidor

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 
 app.set('port',port);
 
@@ -21,4 +21,39 @@ var route = router.get('/',(req,res,next) => {
 app.use('/', route);
 
 server.listen(port);
-console.log('API rodando na porta'+port);
+server.on('error', onError);
+console.log('API rodando na porta  '+port);
+//função normaliza uma porta em um número, string ou false.
+function normalizePort(val){
+    const port = parseInt (val, 10);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port>=0){
+        return port;
+    }
+    return false;
+}
+
+function onError(error){
+
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ? 
+          'Pipe' + port :
+          'Port' + port;
+    switch(error.code) {
+        case 'EACCES':
+            console.log(bind + 'requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDINUSE':  
+            console.log(bind + 'is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
